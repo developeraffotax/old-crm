577df6032257d34cef5d2170b56459673376d030
Salman Sadiq <salmansadiq1015@gmail.com>
2024-06-14 17:31:15 +0500

    friday

--- client/src/utlis/CommonStyle.js
+++
@@ -1,6 +0,0 @@
-export const style = {
-  input:
-    "py-2 px-3 border-2  text-[15px] outline-none border-gray-600 rounded-md shadow-md",
-  button1:
-    "px-[1rem] cursor-pointer rounded-md shadow bg-orange-500 hover:bg-orange-700 transition-all duration-150 text-white flex items-center justify-center py-[.5rem]",
-};
